1968e58ad89db76399bdd34f7d4fcedbb5a94a86
kreedz <edgebug@gmail.com>
2014-03-17 17:03:08 +0600

    wrap long lines

--- pagination.php
+++ pagination.php
@@ -10,7 +10,7 @@ $page -= 1;
 $page *= 10;
 
 if ($stmt = $mysqli->prepare("select u.name, m.message, m.date as mdate, (select count(*) from messages) as mcount,"
-				."m.id as message_id from users u, messages m where m.id_user = u.id order by m.id desc limit ?, 10")) {
+		."m.id as message_id from users u, messages m where m.id_user = u.id order by m.id desc limit ?, 10")) {
 	$stmt->bind_param("d", $page);
 	$stmt->execute();
 	$stmt->bind_result($author, $message, $date, $message_count, $message_id);
